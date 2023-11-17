c1f10579557e292b8a508d3adb71641aba591db5
XandyGomes <x3.computadores@hotmail.com>
2023-11-16 22:27:30 -0300

    Aula.16.11.2023

--- figurinhas/stickers/src/App.java
+++ figurinhas/stickers/src/App.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -12,7 +15,7 @@ public class App {
         // criar criar uma nova imagem em memoria com transparencia e com tamanho novo
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
-        int novaAltura = altura + 200;
+        int novaAltura = altura + 100;
         BufferedImage novaImagem = new BufferedImage(
                 largura, novaAltura, BufferedImage.TYPE_INT_ARGB);
 
@@ -21,7 +24,11 @@ public class App {
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
         // escrever uma mensagem na nova imagem
-
+      
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 35);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.YELLOW);
+        graphics.drawString("OLÁ 2º DSM", 50, novaAltura - 80);
 
         // escrever/gerar a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
